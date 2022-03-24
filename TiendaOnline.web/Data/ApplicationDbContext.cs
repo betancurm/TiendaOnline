9dc6cbd3319b03960dc452b8a59224c0664a7a9e
Juan Betancur <juanbetancur142474@correo.itm.edu.co>
2022-03-24 13:08:28 -0500

    Migración de base de de datos de C# a SQL Server

---
+++ TiendaOnline.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnline.web.Models;
+
+namespace TiendaOnline.web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+            public DbSet<Country> Countries { get; set; }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(t => t.Name).IsUnique();
+        }
+    }
+}
---
+++ TiendaOnline.web/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaOnline.web.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+    }
+}
