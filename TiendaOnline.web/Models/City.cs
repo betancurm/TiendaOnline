87b4d51f7ac8fe7641b942e6b9596276e09f9b20
Juan Manuel Betancur <juanbetancur142474@correo.itm.edu.co>
2022-04-20 09:39:10 -0500

    Iniciamos creando nuevos departamentos, para esto, adicionamos estos métodos al controlador Coutries

--- TiendaOnline.web/Models/City.cs
+++ TiendaOnline.web/Models/City.cs
@@ -1,13 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace TiendaOnline.web.Models
 {
     public class City
     {
-        [Key]
         public int Id { get; set; }
         [MaxLength(50, ErrorMessage = "El campo {0} debe contener al menos un caracter")]
         [Required]
         public string Name { get; set; }
+        [JsonIgnore] //lo ignora en la respuesta json
+        [NotMapped] //no se crea en la base de datos
+        public int IdDepartment { get; set; }
     }
 }
--- TiendaOnline.web/Models/Department.cs
+++ TiendaOnline.web/Models/Department.cs
@@ -2,21 +2,21 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace TiendaOnline.web.Models
 {
     public class Department
     {
-        [Key]
         public int Id { get; set; }
-        [MaxLength(50, ErrorMessage = "El campo {0} debe contener al menos un caracter")]
+        [MaxLength(50)]
         [Required]
         public string Name { get; set; }
         public ICollection<City> Cities { get; set; }
         [DisplayName("Cities Number")]
         public int CitiesNumber => Cities == null ? 0 : Cities.Count;
-
-        [NotMapped]
+        [JsonIgnore] //lo ignora en la respuesta json
+        [NotMapped] //no se crea en la base de datos
         public int IdCountry { get; set; }
     }
 }
